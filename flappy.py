24a2cfa498d57e0f097d1e156f7bfe26468c5298
Luke Schumacher <74542995+luke-schumacher@users.noreply.github.com>
2024-06-08 18:49:53 +0200

    Added Working Scoring System and Flappy Bird Font
    Font is just used for the ''Score" text.

--- flappy.py
+++ flappy.py
@@ -1,7 +1,7 @@
 import pygame, random, time
 from pygame.locals import *
 
-#VARIABLES
+# VARIABLES
 SCREEN_WIDHT = 400
 SCREEN_HEIGHT = 600
 SPEED = 20
@@ -9,7 +9,7 @@ GRAVITY = 2.5
 GAME_SPEED = 15
 
 GROUND_WIDHT = 2 * SCREEN_WIDHT
-GROUND_HEIGHT= 100
+GROUND_HEIGHT = 100
 
 PIPE_WIDHT = 80
 PIPE_HEIGHT = 500
@@ -21,22 +21,16 @@ hit = 'assets/audio/hit.wav'
 
 pygame.mixer.init()
 
-
 class Bird(pygame.sprite.Sprite):
-
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-
-        self.images =  [pygame.image.load('assets/sprites/bluebird-upflap.png').convert_alpha(),
-                        pygame.image.load('assets/sprites/bluebird-midflap.png').convert_alpha(),
-                        pygame.image.load('assets/sprites/bluebird-downflap.png').convert_alpha()]
-
+        self.images = [pygame.image.load('assets/sprites/bluebird-upflap.png').convert_alpha(),
+                       pygame.image.load('assets/sprites/bluebird-midflap.png').convert_alpha(),
+                       pygame.image.load('assets/sprites/bluebird-downflap.png').convert_alpha()]
         self.speed = SPEED
-
         self.current_image = 0
-        self.image = pygame.image.load('assets/sprites/bluebird-upflap.png').convert_alpha()
+        self.image = self.images[self.current_image]
         self.mask = pygame.mask.from_surface(self.image)
-
         self.rect = self.image.get_rect()
         self.rect[0] = SCREEN_WIDHT / 6
         self.rect[1] = SCREEN_HEIGHT / 2
@@ -45,8 +39,6 @@ class Bird(pygame.sprite.Sprite):
         self.current_image = (self.current_image + 1) % 3
         self.image = self.images[self.current_image]
         self.speed += GRAVITY
-
-        #UPDATE HEIGHT
         self.rect[1] += self.speed
 
     def bump(self):
@@ -56,48 +48,33 @@ class Bird(pygame.sprite.Sprite):
         self.current_image = (self.current_image + 1) % 3
         self.image = self.images[self.current_image]
 
-
-
-
 class Pipe(pygame.sprite.Sprite):
-
     def __init__(self, inverted, xpos, ysize):
         pygame.sprite.Sprite.__init__(self)
-
-        self. image = pygame.image.load('assets/sprites/pipe-green.png').convert_alpha()
+        self.image = pygame.image.load('assets/sprites/pipe-green.png').convert_alpha()
         self.image = pygame.transform.scale(self.image, (PIPE_WIDHT, PIPE_HEIGHT))
-
-
         self.rect = self.image.get_rect()
         self.rect[0] = xpos
-
         if inverted:
             self.image = pygame.transform.flip(self.image, False, True)
             self.rect[1] = - (self.rect[3] - ysize)
         else:
             self.rect[1] = SCREEN_HEIGHT - ysize
-
-
         self.mask = pygame.mask.from_surface(self.image)
-
 
     def update(self):
         self.rect[0] -= GAME_SPEED
 
-        
-
 class Ground(pygame.sprite.Sprite):
-    
     def __init__(self, xpos):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load('assets/sprites/base.png').convert_alpha()
         self.image = pygame.transform.scale(self.image, (GROUND_WIDHT, GROUND_HEIGHT))
-
         self.mask = pygame.mask.from_surface(self.image)
-
         self.rect = self.image.get_rect()
         self.rect[0] = xpos
         self.rect[1] = SCREEN_HEIGHT - GROUND_HEIGHT
+
     def update(self):
         self.rect[0] -= GAME_SPEED
 
@@ -109,7 +86,6 @@ def get_random_pipes(xpos):
     pipe = Pipe(False, xpos, size)
     pipe_inverted = Pipe(True, xpos, SCREEN_HEIGHT - size - PIPE_GAP)
     return pipe, pipe_inverted
-
 
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDHT, SCREEN_HEIGHT))
@@ -124,27 +100,23 @@ bird = Bird()
 bird_group.add(bird)
 
 ground_group = pygame.sprite.Group()
-
-for i in range (2):
+for i in range(2):
     ground = Ground(GROUND_WIDHT * i)
     ground_group.add(ground)
 
 pipe_group = pygame.sprite.Group()
-for i in range (2):
+for i in range(2):
     pipes = get_random_pipes(SCREEN_WIDHT * i + 800)
     pipe_group.add(pipes[0])
     pipe_group.add(pipes[1])
 
-
-
 clock = pygame.time.Clock()
-
 begin = True
+score = 0
+font = pygame.font.SysFont(None, 48)
 
 while begin:
-
     clock.tick(15)
-
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
@@ -157,26 +129,18 @@ while begin:
 
     screen.blit(BACKGROUND, (0, 0))
     screen.blit(BEGIN_IMAGE, (120, 150))
-
     if is_off_screen(ground_group.sprites()[0]):
         ground_group.remove(ground_group.sprites()[0])
-
         new_ground = Ground(GROUND_WIDHT - 20)
         ground_group.add(new_ground)
-
     bird.begin()
     ground_group.update()
-
     bird_group.draw(screen)
     ground_group.draw(screen)
-
     pygame.display.update()
 
-
 while True:
-
     clock.tick(15)
-
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
@@ -187,21 +151,24 @@ while True:
                 pygame.mixer.music.play()
 
     screen.blit(BACKGROUND, (0, 0))
-
     if is_off_screen(ground_group.sprites()[0]):
         ground_group.remove(ground_group.sprites()[0])
-
         new_ground = Ground(GROUND_WIDHT - 20)
         ground_group.add(new_ground)
 
     if is_off_screen(pipe_group.sprites()[0]):
         pipe_group.remove(pipe_group.sprites()[0])
         pipe_group.remove(pipe_group.sprites()[0])
-
         pipes = get_random_pipes(SCREEN_WIDHT * 2)
-
         pipe_group.add(pipes[0])
         pipe_group.add(pipes[1])
+    
+    # Check if bird passed the bottom pipe and update score
+    for pipe in pipe_group.sprites():
+        if not hasattr(pipe, 'scored'):
+            if pipe.rect.right < bird.rect.left:
+                pipe.scored = True
+                score += 5
 
     bird_group.update()
     ground_group.update()
@@ -211,6 +178,16 @@ while True:
     pipe_group.draw(screen)
     ground_group.draw(screen)
 
+    # Define font size
+    font_size = 50  # Adjust as needed
+
+    # For "FlappyFont"
+    flappy_font = pygame.font.Font('assets/font/Flappybird.ttf', font_size)
+    score_surface_flappy = flappy_font.render(f'Score: {score}', True, (255, 255, 255))
+    screen.blit(score_surface_flappy, (10, 10))
+
+
+
     pygame.display.update()
 
     if (pygame.sprite.groupcollide(bird_group, ground_group, False, False, pygame.sprite.collide_mask) or
@@ -219,4 +196,3 @@ while True:
         pygame.mixer.music.play()
         time.sleep(1)
         break
-
